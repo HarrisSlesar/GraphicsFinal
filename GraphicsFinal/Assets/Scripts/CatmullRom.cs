980ca4f6e4b19f630199dbfc875f0ea2a90e6d3e
matthew.esslie <matthew.esslie@mymail.champlain.edu>
2021-05-03 17:14:02 -0400

    Commenting and cleaning up code for catmull rom

--- GraphicsFinal/Assets/Scripts/CatmullRom.cs
+++ GraphicsFinal/Assets/Scripts/CatmullRom.cs
@@ -43,11 +43,13 @@ public class CatmullRom : MonoBehaviour
 
         if (paused == false)
         {
+            //setting up duration information and indexes
             durationInv = 1 / segmentDuration;
             int indexStart = currentIndex;
             int indexEnd = (indexStart + 1) % points.Length;
             segmentTime += dt;
 
+            //advancing the indexes if the segment time has passed duration
             if (segmentTime >= segmentDuration)
             {
                 segmentTime -= segmentDuration;
@@ -57,6 +59,7 @@ public class CatmullRom : MonoBehaviour
             currentIndex = indexStart;
             float param = segmentTime * durationInv;
 
+            //setting gameObject position to the position from the algorithm below
             gameObject.transform.position = GetPosition(param, indexStart, indexEnd);
 
            
@@ -70,44 +73,37 @@ public class CatmullRom : MonoBehaviour
     private void OnDrawGizmos() //in case we want to make the catmull rom curve show up on screen
     {
         Gizmos.color = Color.white;
-
-        for (int i = 0; i < points.Length; i++)
-        {
-            DisplayCatmullRom(i);
-        }
+      
+        DisplayCatmullRom();        
     }
 
     
-    void DisplayCatmullRom(int val)
+    void DisplayCatmullRom()
     {
+        //this function draws the lines on screen for better visuals with the catmull rom
         Vector3 p0 = points[0].position;
         Vector3 p1 = points[1].position;
         Vector3 p2 = points[2].position;
         Vector3 p3 = points[3].position;
-        lastPosition = p1; //p0 is sort of the starting previous position? idk
+        lastPosition = p1; 
 
         //newPosition = GetPosition(p0, p1, p2, p3);
-
-        //Gizmos.DrawLine(points[0].position, points[1].position);
-        //Gizmos.DrawLine(points[1].position, points[2].position);
-        //Gizmos.DrawLine(points[2].position, points[3].position);
-        //Gizmos.DrawLine(points[3].position, points[0].position);
 
         for (int i = 0; i < maxSegmentAmount - 1; i++)
         {
             Gizmos.DrawLine(points[i].position, points[i + 1].position);
         }
 
+        //drawing the final connecting line since the for loop does not include it
         Gizmos.DrawLine(points[maxSegmentAmount - 1].position, points[0].position);
-
-        //lastPosition = newPosition;
     }
     
     Vector3 GetPosition(float param, int start, int end)
     {
+        //getting the number we need to for running modulus
         int num = points.Length;
 
-
+        //setting up the previous, current, future and next points for the catmull process
         Vector3 p0 = points[Mod((start-1),num)].position;
         Vector3 p1 = points[start].position;
         Vector3 p2 = points[end].position;
@@ -115,25 +111,21 @@ public class CatmullRom : MonoBehaviour
 
         //time = time / 2; //this was to slow down the orbit for more viewability
 
-        //float t0 = 0.0f;
-        //float t1 = t0 + Mathf.Pow(p0.magnitude - p1.magnitude, alpha);
-        //float t2 = t1 + Mathf.Pow(p1.magnitude - p2.magnitude, alpha);
-        //float t3 = t2 + Mathf.Pow(p2.magnitude - p3.magnitude, alpha);
-
-        //tmp1 = p0 * time * time * time;
-
+        //this is our current algorithm to run catmull rom 
         Vector3 currentPosition = ((-0.5f * p0 + 1.5f * p1 - 1.5f * p2 + 0.5f * p3) * (param * param * param)
            + (1f * p0 - 2.5f * p1 + 2f * p2 - 0.5f * p3) * (param * param)
            + (-0.5f * p0 + 0.5f * p2) * param
-           + 1f * p1); //this was the old algorithm I was testing
+           + 1f * p1);
 
+        //old algorithm for catmull rom
         //Vector3 currentPosition = alpha * ((2 * p1) + (-p0 + p2) * time + (2 * p0 - 5 * p1 + 4 * p2 - p3)
-         // * (time * time) + (-p0 + 3 * p1 - 3 * p2 + p3) * (time * time * time)); //current algorithm for catmull rom
+        // * (time * time) + (-p0 + 3 * p1 - 3 * p2 + p3) * (time * time * time));
 
+        //returning the new point for the planet to move to
         return currentPosition;
     }
 
-
+    //our modulus function since % runs as a remainder not modulus in C#
     int Mod(int a, int b)
     {
         return (a % b + b) % b;
